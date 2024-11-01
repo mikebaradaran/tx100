03f2718bfbf2c10cd6a5bea225341c4b386ddf9e
Glitch (tx100) <none>
2024-11-01 09:27:48 +0000

    🤲🕋 Updated with Glitch 2

--- server.js
+++ server.js
@@ -42,52 +42,7 @@ app.get("/start", (req, res) => {
   res.render("txStart");
 });
 app.post("/startSubmit", (req, res) => {
-  var  {
-    audio,
-    trainer,
-    course_title,
-    code,
-    pin,
-    webex_email,
-    material,
-    mimeo,
-    pcs,
-    password1,
-    password2,
-    password3,
-    password4,
-    students,
-  } = req.body;
-  
-  pcs = pcs.replace(/\r/g, ''); 
-  students = students.replace(/\r/g, ''); 
-  students = students.replace(/\t/g, ''); 
-  
-  const formData = {
-    audio:audio,
-    trainer: trainer,
-    course_title: course_title,
-    code: code,
-    pin: pin,
-    webex_email: webex_email,
-    material: material,
-    mimeo: mimeo,
-    pcs: pcs,
-    password1: password1,
-    password2: password2,
-    password3: password3,
-    students: students
-  };
-
-  fs.writeFile("data.json", JSON.stringify(formData, null, 2), (err) => {
-    if (err) {
-      console.error("Error writing to file:", err);
-      res.status(500).send("Error saving data");
-    } else {
-      console.log("Data saved successfully.");
-      res.render("index");
-    }
-  });
+  initApp(req,res);
 });
 
 app.get("/startRead", (req, res) => {
@@ -198,7 +153,54 @@ server.listen(
 );
 
 //------------------------------------------------------------
+function initApp(req,res){
+   var  {
+    audio,
+    trainer,
+    course_title,
+    code,
+    pin,
+    webex_email,
+    material,
+    mimeo,
+    pcs,
+    password1,
+    password2,
+    password3,
+    password4,
+    students,
+  } = req.body;
+  
+  pcs = pcs.replace(/\r/g, ''); 
+  students = students.replace(/\r/g, ''); 
+  students = students.replace(/\t/g, ''); 
+  
+  const formData = {
+    audio:audio,
+    trainer: trainer,
+    course_title: course_title,
+    code: code,
+    pin: pin,
+    webex_email: webex_email,
+    material: material,
+    mimeo: mimeo,
+    pcs: pcs,
+    password1: password1,
+    password2: password2,
+    password3: password3,
+    students: students
+  };
 
+  fs.writeFile("data.json", JSON.stringify(formData, null, 2), (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+      res.status(500).send("Error saving data");
+    } else {
+      console.log("Data saved successfully.");
+      res.render("index");
+    }
+  });
+}
 var messages = [];
 
 function doTrainerCommand(data) {
