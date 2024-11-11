0a2d656cef53db28b16e862f013190678ab9dee1
Glitch (tx100) <none>
2024-11-11 01:08:47 +0000

    🦛🎽 Updated with Glitch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const commentJS = require("./comments.js");
 const commonData = require("./common.js");
+const serverUtils = require("./serverUtils.js");
 
 const fs = require("fs");
 const cors = require("cors");
@@ -43,12 +44,12 @@ app.get("/start", (req, res) => {
   res.render("txStart");
 });
 app.post("/startSubmit", (req, res) => {
-  initApp(req, res, fs);
+  serverUtils.initApp(req, res, fs);
 });
 
 app.get("/startRead", (req, res) => {
   let data = fs.readFileSync("data.json", "utf8");
-  res.send(processStartData(data));
+  res.send(serverUtils.processStartData(data));
 });
 
 app.get("/index", (req, res) => {
@@ -211,61 +212,3 @@ io.on("connection", (socket) => {
 //   });
 // });
 
-function initApp(req, res, fs) {
-  var {
-    audio,
-    trainer,
-    course_title,
-    code,
-    pin,
-    webex_email,
-    material,
-    mimeo,
-    pcs,
-    password1,
-    password2,
-    password3,
-    password4,
-    students,
-  } = req.body;
-
-  pcs = pcs.replace(/\r/g, "");
-  students = students.replace(/\r/g, "");
-  students = students.replace(/\t/g, "");
-
-  const formData = {
-    audio: audio,
-    trainer: trainer,
-    course_title: course_title,
-    code: code,
-    pin: pin,
-    webex_email: webex_email,
-    material: material,
-    mimeo: mimeo,
-    pcs: pcs,
-    password1: password1,
-    password2: password2,
-    password3: password3,
-    students: students,
-  };
-
-  fs.writeFile("data.json", JSON.stringify(formData, null, 2), (err) => {
-    if (err) {
-      console.error("Error writing to file:", err);
-      res.status(500).send("Error saving data");
-    } else {
-      console.log("Data saved successfully.");
-      res.render("index");
-    }
-  });
-}
-
-function processStartData(data){
-  data = JSON.parse(data);
-  data.pcs = data.pcs;
-  data.students = data.students.replace("(REQS)", '');
-  data.students = data.students.replace("\t", '');
-  data.pcs = data.pcs.split("\n");
-  data.students = data.students.split("\n");
-  return data;
-}
