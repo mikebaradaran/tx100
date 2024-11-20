393a50e1fc567042bb7b2de38add933b91d25251
Glitch (txbob2) <none>
2024-11-20 15:42:27 +0000

    🌇🚟 Updated with Glitch

--- public/index.js
+++ public/index.js
@@ -19,7 +19,8 @@ function copy(str) {
 
 function setupForm() {
   const evalLink = `https://evaluation.qa.com/Login.aspx?course=${courseData.code}&pin=${courseData.pin}`;
-  
+
+  getTimer().sound = courseData.audio;
   getElement("trainer").innerHTML = courseData.trainer;
   getElement("course_title").innerHTML = courseData.course_title;
   getElement("material").href = courseData.material;
@@ -27,13 +28,35 @@ function setupForm() {
   // setup combobox
   const cboValues = [
     { title: "Select an item", msg: "", timer: 0 },
-    { title: "Finish lab", msg: "Please put a ✔ when you have completed the lab", timer: -1},
-    { title: "ready to start", msg: "Please put a ✔ when you are ready to start 🏍", timer: -1},
+    {
+      title: "Finish lab",
+      msg: "Please put a ✔ when you have completed the lab",
+      timer: -1,
+    },
+    {
+      title: "ready to start",
+      msg: "Please put a ✔ when you are ready to start 🏍",
+      timer: -1,
+    },
     { title: "Coffee", msg: "Let's take a 15 minutes break ☕", timer: 15 },
     { title: "Lunch", msg: "Let's take 60 minutes for lunch 🍔", timer: 60 },
-    { title: "mini break", msg: "Let's take a 5 minutes mini break ☕", timer: 5 },
-    { title: "Comment", msg: "Please write comments about the course", link: `${site}/comments`, timer: -1},
-    { title: "Evaluation", msg: "Please complete the course evaluation", link: evalLink, timer: -1}
+    {
+      title: "mini break",
+      msg: "Let's take a 5 minutes mini break ☕",
+      timer: 5,
+    },
+    {
+      title: "Comment",
+      msg: "Please write comments about the course",
+      link: `${site}/comments`,
+      timer: -1,
+    },
+    {
+      title: "Evaluation",
+      msg: "Please complete the course evaluation",
+      link: evalLink,
+      timer: -1,
+    },
   ];
 
   var cboMessages = getElement("cboMessages");
@@ -52,20 +75,22 @@ function setupForm() {
     if (cboTime == -1) return;
 
     if (cboTime == 0) {
-      stopTimer();
-      getElement("divTimer").innerHTML = "";
-      setMessage("");
+      getTimer().stopTimer();
+      getTimer().message = "";
     } else {
-      getElement("timer").value = cboTime;
-      startTimer("timer", "divTimer");
+      getTimer().timerValue = cboTime * 60;
+      getTimer().startTimer();
     }
   });
-// End of setting up combobox --------------------------------------------- 
-  
-  courseData.students = ["Trainer, Trainer", ...courseData.students]
-  console.log(courseData.students);
+
+  function getTimer() {
+    return document.querySelector("#qaTimer");
+  }
+  // End of setting up combobox ---------------------------------------------
+
+  courseData.students = ["Trainer, Trainer", ...courseData.students];
   courseData.students.forEach((stu, i) => {
-    if(stu.length !== 0){
+    if (stu.length !== 0) {
       var ol = getElement("pcs");
       var li = document.createElement("li");
       var a = document.createElement("a");
@@ -92,42 +117,4 @@ function afa() {
 
 function getElement(id) {
   return document.getElementById(id);
-}
-//========================Timer and messages==============================
-var myTimer = null;
-function stopTimer() {
-  if (myTimer !== null) clearInterval(myTimer);
-}
-function startTimer(timerName, divCountdown) {
-  let mins = parseInt(getElement(timerName).value);
-  console.log(mins);
-  if (mins < 0) return;
-
-  stopTimer();
-  setMessage("");
-
-  let seconds = mins * 60;
-
-  myTimer = setInterval(function () {
-    var minutes = (seconds / 60) | 0;
-
-    if (seconds < 0) {
-      stopTimer();
-      setMessage(mins + " minutes passed. Ended at " + getTime());
-      new Audio(courseData.audio).play();
-      return;
-    }
-    getElement(divCountdown).innerHTML =
-      minutes + ":" + (seconds - minutes * 60);
-    seconds--;
-  }, 1000);
-}
-
-function setMessage(msg) {
-  document.getElementById("message").innerHTML = msg;
-}
-
-function getTime() {
-  var today = new Date();
-  return today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 }
