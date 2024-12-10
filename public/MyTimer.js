1b3219aebcee20350f510d8f7054fe9489426e72
Glitch (tx100) <none>
2024-12-10 22:10:06 +0000

    🧻😯 Updated with Glitch

--- public/MyTimer.js
+++ public/MyTimer.js
@@ -5,12 +5,18 @@ class QA_Timer extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
         <link href="MyTimer.css" rel="stylesheet" />
-          <span id="break">🍵</span>
-          <span id="lunch">🍕</span>
           <span id="timerGoButton">▶️</span>
           <input id="timer" type="range" min="1" max="120"/>
           <span id="info"></span>
         </p>`;
+        // this.shadowRoot.innerHTML = `
+        // <link href="MyTimer.css" rel="stylesheet" />
+        //   <span id="break">🍵</span>
+        //   <span id="lunch">🍕</span>
+        //   <span id="timerGoButton">▶️</span>
+        //   <input id="timer" type="range" min="1" max="120"/>
+        //   <span id="info"></span>
+        // </p>`;
   }
   timerGoButton_click() {
     let t = document.querySelector("#qaTimer");
@@ -22,8 +28,8 @@ class QA_Timer extends HTMLElement {
     this.timer.addEventListener("input", this.sliding.bind(this));
     const btnGo= this.shadowRoot.querySelector("#timerGoButton");
     btnGo.addEventListener("click", this.timerGoButton_click.bind(this));
-    this.shadowRoot.querySelector("#break").addEventListener("click", ()=>{this.timerValue=(15*60); this.startTimer()});
-    this.shadowRoot.querySelector("#lunch").addEventListener("click", ()=>{this.timerValue=(60*60); this.startTimer()});
+    // this.shadowRoot.querySelector("#break").addEventListener("click", ()=>{this.timerValue=(15*60); this.startTimer()});
+    // this.shadowRoot.querySelector("#lunch").addEventListener("click", ()=>{this.timerValue=(60*60); this.startTimer()});
   }
 
   sliding() {
