33bdd568bc07899919adccf255124ea7b5bb24a1
Mike Baradaran <mike.baradaran@qa.com>
2025-06-02 09:10:29 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -18,24 +18,47 @@ function setupForm(data) {
   const evalLink = `https://evaluation.qa.com/Login.aspx?course=${courseData.code}&pin=${courseData.pin}`;
 
   getElement("qaTimer").sound = courseData.audio;
-  getElement("trainer").innerHTML = courseData.trainer + ` - (${courseData.courseDuration} days)`;
+  getElement("trainer").innerHTML =
+    courseData.trainer + ` - (${courseData.courseDuration} days)`;
   getElement("course_title").innerHTML = courseData.course_title;
   getElement("material").href = courseData.material;
 
   // setup combobox
   const cboValues = [
     { title: "Select an item", msg: "", timer: 0 },
-    { title: "Finish lab",     msg: "Please put a ✔ when you have completed the lab", timer: -1 },
-    { title: "ready to start", msg: "Please put a ✔ when you are ready to start 🏍", timer: -1  },
+    {
+      title: "Finish lab",
+      msg: "Please put a ✔ when you have completed the lab",
+      timer: -1,
+    },
+    {
+      title: "ready to start",
+      msg: "Please put a ✔ when you are ready to start 🏍",
+      timer: -1,
+    },
     { title: "Coffee", msg: "Let's take a 15 minutes break ☕", timer: 15 },
     { title: "Lunch", msg: "Let's take 60 minutes for lunch 🍔", timer: 60 },
-    { title: "mini break", msg: "Let's take a 5 minutes mini break ☕", timer: 5 },
-    { title: "Comment",    msg: "Please write comments about the course", link: `/comments`, timer: -1},
-    { title: "Evaluation", msg: "Please complete the course evaluation",  link: evalLink, timer: -1 },
+    {
+      title: "mini break",
+      msg: "Let's take a 5 minutes mini break ☕",
+      timer: 5,
+    },
+    {
+      title: "Comment",
+      msg: "Please write comments about the course",
+      link: `/comments`,
+      timer: -1,
+    },
+    {
+      title: "Evaluation",
+      msg: "Please complete the course evaluation",
+      link: evalLink,
+      timer: -1,
+    },
   ];
 
   var cboMessages = getElement("cboMessages");
-  
+
   cboValues.forEach((x) => {
     var op = document.createElement("option");
     op.innerHTML = x.title;
@@ -45,14 +68,12 @@ function setupForm(data) {
   cboMessages.addEventListener("change", () => {
     let i = cboMessages.selectedIndex;
     getElement("txtArea").value = cboValues[i].msg;
-    if (cboValues[i].link) 
-      window.open(cboValues[i].link, "_blank");
+    if (cboValues[i].link) window.open(cboValues[i].link, "_blank");
 
     let duration = cboValues[i].timer;
 
-    if (duration == -1)
-      return;
-    
+    if (duration == -1) return;
+
     let qaTimer = getElement("qaTimer");
 
     if (duration == 0) {
@@ -63,6 +84,21 @@ function setupForm(data) {
       qaTimer.startTimer();
     }
   });
+  
+  
+  if (courseData.password1.length < 2)   
+    document.getElementsByName("passwords")[0].style.visibility = "hidden";  
+  if (courseData.password2.length < 2) 
+    document.getElementsByName("passwords")[1].style.visibility = "hidden";  
+  if (courseData.mimeo.length < 2) 
+    getElement("mimeo").style.visibility = "hidden";  
+    
+    
+  // if (courseData.password1.length < 2) {   
+  //   document.getElementsByName("passwords")[0].style.visibility = "hidden";  
+  //   document.getElementsByName("passwords")[1].style.visibility = "hidden";  
+  //   document.getElementsByName("passwords")[2].style.visibility = "hidden";  
+  // }
 
   // End of setting up combobox ---------------------------------------------
 
@@ -79,7 +115,6 @@ function setupForm(data) {
       ol.appendChild(li);
     }
   });
-
 }
 
 function copy(str) {
@@ -94,9 +129,7 @@ function getElement(id) {
   return document.getElementById(id);
 }
 
-function mimeo(){
+function mimeo() {
   copy(courseData.mimeo);
   window.open("https://mimeo.digital/QALtd/distributions", "_blank");
 }
-
-
