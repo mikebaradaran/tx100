87f97c079d6824fdfada37ec01f7a832b90a6731
mike Baradaran <mike.baradaran@qa.com>
2026-03-26 17:22:00 +0000

    made mobile

--- public/index.js
+++ public/index.js
@@ -18,9 +18,9 @@ function setupForm(data) {
   const evalLink = `https://evaluation.qa.com/Login.aspx?course=${courseData.code}&pin=${courseData.pin}`;
 
   getElement("qaTimer").sound = courseData.audio;
-  getElement("trainer").innerHTML =
-    courseData.trainer + ` - (${courseData.courseDuration} days)`;
-  getElement("course_title").innerHTML = courseData.course_title;
+  // getElement("trainer").innerHTML =
+  //   courseData.trainer + ` - (${courseData.courseDuration} days)`
+  getElement("course_title").innerHTML = `${courseData.course_title} <div id='trainer'> ${courseData.trainer} - ${courseData.courseDuration} days</div>`;
   getElement("material").href = courseData.material;
 
   // setup combobox
