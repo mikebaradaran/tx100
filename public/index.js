c8566042a90b9b01bb5fca1ce4c50e575f4992ec
mike Baradaran <mike.baradaran@qa.com>
2026-03-27 00:54:22 +0000

    Cleaned up cboMessages

--- public/index.js
+++ public/index.js
@@ -18,89 +18,48 @@ function setupForm(data) {
   const evalLink = `https://evaluation.qa.com/Login.aspx?course=${courseData.code}&pin=${courseData.pin}`;
 
   getElement("qaTimer").sound = courseData.audio;
-  // getElement("trainer").innerHTML =
-  //   courseData.trainer + ` - (${courseData.courseDuration} days)`
   getElement("course_title").innerHTML = `${courseData.course_title} <div id='trainer'> ${courseData.trainer} - ${courseData.courseDuration} days</div>`;
   getElement("material").href = courseData.material;
 
   // setup combobox
-  const cboValues = [
-    { title: "Select an item", msg: "", timer: 0 },
-    {
-      title: "Finish lab",
-      msg: "Please put a ✔ when you have completed the lab",
-      timer: -1,
-    },
-    {
-      title: "ready to start",
-      msg: "Please put a ✔ when you are ready to start 🏍",
-      timer: -1,
-    },
-    { title: "Coffee", msg: "Let's take a 15 minutes break ☕", timer: 15 },
-    { title: "Lunch", msg: "Let's take 60 minutes for lunch 🍔", timer: 60 },
-    {
-      title: "mini break",
-      msg: "Let's take a 5 minutes mini break ☕",
-      timer: 5,
-    },
-    {
-      title: "Comment",
-      msg: "Please write comments about the course",
-      link: `/comments`,
-      timer: -1,
-    },
-    {
-      title: "Evaluation",
-      msg: "Please complete the course evaluation",
-      link: evalLink,
-      timer: -1,
-    },
-  ];
 
-  var cboMessages = getElement("cboMessages");
+  function cboMessage_onchange() {
+    const qaTimer = document.getElementById("qaTimer");
+    const cboMessages = document.getElementById("cboMessages");
+    const txtArea = document.getElementById("txtArea");
 
-  cboValues.forEach((x) => {
-    var op = document.createElement("option");
-    op.innerHTML = x.title;
-    cboMessages.appendChild(op);
-  });
+    const selectedOption = cboMessages.options[cboMessages.selectedIndex];
+    const link = selectedOption.getAttribute('link');
+    const timerValue = selectedOption.getAttribute('timer');
 
-  cboMessages.addEventListener("change", () => {
-    let i = cboMessages.selectedIndex;
-    getElement("txtArea").value = cboValues[i].msg;
-    if (cboValues[i].link) window.open(cboValues[i].link, "_blank");
+    txtArea.value = selectedOption.getAttribute('msg');
 
-    let duration = cboValues[i].timer;
-
-    if (duration == -1) return;
-
-    let qaTimer = getElement("qaTimer");
-
-    if (duration == 0) {
-      qaTimer.stopTimer();
-      qaTimer.message = "";
-    } else {
-      qaTimer.timerValue = duration * 60;
-      qaTimer.startTimer();
+    if (timerValue) {
+      // qaTimer.innerText = parseInt(timerValue, 10) * 60;
+      if (timerValue == 0) {
+        qaTimer.stopTimer();
+        qaTimer.message = "";
+      } else {
+        qaTimer.timerValue = duration * 60;
+        qaTimer.startTimer();
+      }
     }
-  });
-  
-  
-  if (courseData.password1.length < 2)   
-    document.getElementsByName("passwords")[0].style.visibility = "hidden";  
-  if (courseData.password2.length < 2) 
-    document.getElementsByName("passwords")[1].style.visibility = "hidden";  
-  if (courseData.mimeo.length < 2) 
-    getElement("mimeo").style.visibility = "hidden";  
-    
-    
-  // if (courseData.password1.length < 2) {   
-  //   document.getElementsByName("passwords")[0].style.visibility = "hidden";  
-  //   document.getElementsByName("passwords")[1].style.visibility = "hidden";  
-  //   document.getElementsByName("passwords")[2].style.visibility = "hidden";  
-  // }
-
+    if (link) {
+      if (link === "evaluation") {
+        window.open(evalLink, "_blank");
+      }
+    }
+  }
   // End of setting up combobox ---------------------------------------------
+
+
+  if (courseData.password1.length < 2)
+    document.getElementsByName("passwords")[0].style.visibility = "hidden";
+  if (courseData.password2.length < 2)
+    document.getElementsByName("passwords")[1].style.visibility = "hidden";
+  if (courseData.mimeo.length < 2)
+    getElement("mimeo").style.visibility = "hidden";
+
 
   courseData.students = ["Trainer", ...courseData.students];
   courseData.students.forEach((stu, i) => {
