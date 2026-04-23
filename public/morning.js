6bda302bfdf577cee334887d86a6613e2c79d74c
mike Baradaran <mike.baradaran@qa.com>
2026-04-23 11:58:37 +0100

    modified Morning view

--- public/morning.js
+++ public/morning.js
@@ -1,35 +1,3 @@
-fetch("/start/Read")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    setupForm(data);
-  })
-  .catch(function (error) {
-    alert(error);
-  });
-
-function setupForm(courseData) {
-  const evalLink = `https://evaluation.qa.com/Login.aspx?course=${courseData.code}&pin=${courseData.pin}`;
-
-  //getElement("qaTimer").sound = courseData.audio;
-  getElement("trainer").innerHTML = courseData.trainer;
-  getElement("course_title").innerHTML = courseData.course_title;
-
-  courseData.students.forEach((stu, i) => {
-    if (stu.length !== 0) {
-      var ol = getElement("pcs");
-      var li = document.createElement("li");
-      li.innerText = stu;
-      ol.appendChild(li);
-    }
-  });
-}
-
-function getElement(id) {
-  return document.getElementById(id);
-}
-
 let futureTime = new Date();
 futureTime.setHours(9);
 futureTime.setMinutes(30);
@@ -40,7 +8,6 @@ let secondsToStart = Math.floor((futureTime - new Date()) / 1000);
 if (secondsToStart > 0) {
   let timer = document.getElementById("qaTimer");
   timer.timerValue = secondsToStart;
-  timer.start();
+  timer.start(secondsToStart);  
 }
-document.getElementById("txtArea").value =
-  "We'll be starting class at 9:30\nPlease make sure you're ready on time";
+
